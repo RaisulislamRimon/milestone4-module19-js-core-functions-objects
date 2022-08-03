6549cf41df5c01071ac07da1c78d4b65f1874ac5
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-03 19:35:24 +0000

    parameter undefined

--- module19-3/1 parameter.js
+++ module19-3/1 parameter.js
@@ -3,8 +3,16 @@
 // }
 // showNumber(1);
 
-function add(num1, num2) {
-  console.log(num1 + num2);
+// function add(num1, num2) {
+//   console.log(num1 + num2);
+// }
+
+// add(1, 2);
+
+function sum(a, b, c, d, e) {
+  console.log(a, b, c, d, e);
+  console.log(a + b + c + d + e);
 }
 
-add(1, 2);
+// sum(10, 20, 30, 40, 50);
+sum(10, 20, 30); // it will show 40 and 50 as undefined
