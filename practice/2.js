1c2a28ad4c1c16a36d8743107a8d3ce853f9b948
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-04 13:12:56 +0000

    questions added

--- practice/2.js
+++ practice/2.js
@@ -1,3 +1,7 @@
+/* 
+2) Write a function called make_avg() which will take an three integers and return the
+average of those values. */
+
 function make_avg(num1, num2, num3) {
   var total = num1 + num2 + num3;
   var avg = total / 3;
