1cc1505fbfab6b835089c64c0beff654cf6a7050
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-04 10:30:13 +0000

    function

--- module19-4/1 return.js
+++ module19-4/1 return.js
@@ -1,11 +1,26 @@
-// return function
-function add(a, b) {
-  console.log(a, b);
-  var sum = a + b;
+/* function add(num1, num2) {
+  console.log(num1, num2);
+  var sum = num1 + num2;
+  console.log("I need more code");
   return sum;
+  // return "hello done";
+  // return "i am hungry";
+  // return 15;
+  // console.log(sum);
+  // return 15;
 }
 
-add(1, 2); // 3
-console.log(); // 3
+// add(45,15);
+var total = add(80, 20);
+console.log(total);
+ */
 
-// 19-4 Everything you need to know about return from a function
+function bringSingara(money) {
+  var singaraPrice = 10;
+  var quantity = money / singaraPrice;
+  return quantity;
+}
+
+var myTaka = 150;
+var singaras = bringSingara(myTaka);
+console.log(singaras);
