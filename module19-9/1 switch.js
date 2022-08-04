98ea0ae60b43d6c40c41fe5b48caa109ada30c0e
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-04 13:03:42 +0000

    switch

---
+++ module19-9/1 switch.js
@@ -0,0 +1,37 @@
+const color = "";
+// const color = "white";
+
+if (color === "green") {
+  console.log("green color");
+} else if (color === "blue") {
+  console.log("blue color");
+} else if (color === "red") {
+  console.log("red color");
+} else if (color === "white") {
+  console.log("white color");
+} else {
+  console.log("black color");
+}
+
+// switch
+var color1 = "pink";  
+switch (color1) {
+  case "white":
+    console.log("white color");
+    break;
+  case "green":
+    console.log("green color");
+    break;
+  case "blue":
+    console.log("blue color");
+    break;
+  case "red":
+    console.log("red color");
+    break;
+  case "white":
+    console.log("white color");
+    break;
+  default:
+    console.log("black color");
+    break;
+}
