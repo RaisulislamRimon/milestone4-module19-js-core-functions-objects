8e54ba5ba1c30971164767f0b5112f015c41ff55
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-10 15:17:37 +0600

    Update 1.js

--- practice/1.js
+++ practice/1.js
@@ -3,3 +3,13 @@
 “bar”. Call function bar() in the foo() function after printing. What will be the output? Now call
 the foo() to see the output.
  */
+function foo() {
+  console.log("foo");
+  bar();
+}
+
+function bar() {
+  console.log("bar");
+}
+
+foo();
