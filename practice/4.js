54e06baf943770442c9eac1d0dfe5a208053a919
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-10 15:31:41 +0600

    Update 4.js

--- practice/4.js
+++ practice/4.js
@@ -4,3 +4,32 @@ value is even or odd. You need to do it in 4 ways:
 ● Has return + Has parameter
 ● No return + Has parameter
  */
+
+
+// ----------------------------------------
+// ● Has return + Has parameter
+// ----------------------------------------
+function odd_even(num) {
+  if (num % 2 == 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+
+console.log(odd_even(2));
+console.log(odd_even(3));
+
+// ----------------------------------------
+// ● No return + Has parameter
+// ----------------------------------------
+function odd_even1(num1) {
+  if (num1 % 2 == 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+}
+
+odd_even1(2);
+odd_even1(3);
