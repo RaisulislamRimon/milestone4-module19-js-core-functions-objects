81446d91837e09d4420dd89da6f7fd08cf1724c6
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-10 15:25:56 +0600

    Update 3.js

--- practice/3.js
+++ practice/3.js
@@ -2,3 +2,12 @@
 3) Challenging: Write a function called make_avg() which will take an array of integers and
 the size of that array and return the average of those values.
  */
+function make_avg(arr, size) {
+  var sum = 0;
+  for (var i = 0; i < size; i++) {
+    sum += arr[i];
+  }
+  return sum / size;
+}
+
+console.log(make_avg([1, 2, 3, 4, 5], 5));
